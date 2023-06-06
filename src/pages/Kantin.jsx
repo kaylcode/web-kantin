e388c46c1a83118b7b74f08b23558617b27c5344
kaylcode <aisyahnurkayla1@gmail.com>
2023-06-06 10:17:20 +0700

    update

--- src/pages/Kantin.jsx
+++ src/pages/Kantin.jsx
@@ -60,7 +60,7 @@ function Kantin() {
         </a>
         <div class="p-5">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Juice
+            Star Juice
           </h5>
 
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
